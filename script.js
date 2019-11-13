6ba1fff8892e4320d1d34c577c8ae0fd49484379
adamgparsons <adamgeorgeparsons@gmail.com>
2019-11-13 16:04:00 +0000

    fixed CORS issue

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const getShibas = function() {
-  fetch("https://cors.io/https://shibe.online/api/shibes?count=5")
-    .then(response => response.json())
-    .then(jsonData => console.log(jsonData));
-};
-
-getShibas();
+fetch(
+  "https://cors-anywhere.herokuapp.com/http://shibe.online/api/shibes?count=5"
+)
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    // Work with JSON data here
+    console.log("data", data);
+  })
+  .catch(err => {
+    // Do something for an error here
+  });

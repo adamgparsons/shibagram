b573118ea4802cf9d5288dfd5efd0f8a72edfea7
adamgparsons <adamgeorgeparsons@gmail.com>
2019-11-14 10:16:53 +0000

    updated name and location

--- script.js
+++ script.js
@@ -1,68 +1,87 @@
+import japanCities from "./japanCities";
+import dogNames from "./dogNames";
 const mainTag = document.querySelector("main");
 
-const getShibas = function() {
-  let shiba = "";
-  fetch(
-    "https://cors-anywhere.herokuapp.com/http://shibe.online/api/shibes?count=5"
-  )
-    .then(displayPics => {
-      return displayPics.json();
+//get shibas
+// first first image the first image
+// store it in a variable called DisplayImage
+//
+
+const getShiba = function() {
+  let displayPic = "";
+  let postPic = "";
+  const dogName = dogNames[Math.floor(Math.random() * dogNames.length)];
+  const postLocation =
+    japanCities[Math.floor(Math.random() * japanCities.length)] + ", Japan";
+  console.log(postLocation);
+  fetch("https://cors-anywhere.herokuapp.com/http://shibe.online/api/shibes?")
+    .then(result => {
+      return result.json();
     })
-    .then(data => {
-      const convertArrayToObject = (array, key) =>
-        array.reduce(
-          (obj, item) => ({
-            ...obj,
-            [item[key]]: item
-          }),
-          {}
-        );
-      console.log(convertArrayToObject(data));
-
-      //   console.log(test);
-      //   data.forEach(imgUrl => {
-      //     // Work with JSON data here
-      //     const shibaPost = document.createElement("div");
-      //     shibaPost.className = "shiba-post";
-      //     mainTag.appendChild(shibaPost);
-
-      //     const profileBar = document.createElement("div");
-      //     profileBar.className = "profile-bar";
-      //     shibaPost.appendChild(profileBar);
-
-      //     const profileImgHolder = document.createElement("div");
-      //     profileImgHolder.className = "profile-img-holder";
-      //     profileBar.appendChild(profileImgHolder);
-
-      //     const profileImg = document.createElement("div");
-      //     profileImg.className = "profile-img";
-      //     profileImgHolder.appendChild(profileImg);
-
-      //     const profileDetails = document.createElement("div");
-      //     profileDetails.className = "profile-details";
-      //     profileBar.append(profileDetails);
-
-      //     const shibaName = document.createElement("h2");
-      //     shibaName.innerText = "Test Shiba Name";
-      //     profileDetails.appendChild(shibaName);
-
-      //     const shibaLocation = document.createElement("h3");
-      //     shibaLocation.innerText = "Test Shiba Location";
-      //     profileDetails.appendChild(shibaLocation);
-
-      //     const imgFrame = document.createElement("div");
-      //     imgFrame.className = "img-frame";
-      //     shibaPost.appendChild(imgFrame);
-
-      //     const mainImg = document.createElement("img");
-      //     mainImg.className = "main-img";
-      //     mainImg.src = imgUrl;
-      //     imgFrame.appendChild(mainImg);
-      //   });
+    .then(jsonData => {
+      displayPic = jsonData.toString();
+      console.log(displayPic);
+    });
+  fetch("https://cors-anywhere.herokuapp.com/http://shibe.online/api/shibes?")
+    .then(result => {
+      return result.json();
+    })
+    .then(jsonData => {
+      postPic = jsonData.toString();
+      if (displayPic.length > 0) {
+        generatePost(displayPic, postPic, dogName, postLocation);
+      }
     })
     .catch(err => {
       // Do something for an error here
     });
+
+  const generatePost = function(
+    displayPicUrl,
+    postPicUrl,
+    dogName,
+    postLocation
+  ) {
+    const shibaPost = document.createElement("div");
+    shibaPost.className = "shiba-post";
+    mainTag.appendChild(shibaPost);
+
+    const profileBar = document.createElement("div");
+    profileBar.className = "profile-bar";
+    shibaPost.appendChild(profileBar);
+
+    const profileImgHolder = document.createElement("div");
+    profileImgHolder.className = "profile-img-holder";
+    profileBar.appendChild(profileImgHolder);
+
+    const profileImg = document.createElement("div");
+    profileImg.className = "profile-img";
+    profileImg.style.backgroundImage = `url(${displayPicUrl})`;
+    profileImgHolder.appendChild(profileImg);
+
+    const profileDetails = document.createElement("div");
+    profileDetails.className = "profile-details";
+    profileBar.append(profileDetails);
+
+    const shibaName = document.createElement("h2");
+    shibaName.innerText = dogName;
+    profileDetails.appendChild(shibaName);
+
+    const shibaLocation = document.createElement("h3");
+    shibaLocation.innerText = postLocation;
+    profileDetails.appendChild(shibaLocation);
+
+    const imgFrame = document.createElement("div");
+    imgFrame.className = "img-frame";
+    shibaPost.appendChild(imgFrame);
+
+    const mainImg = document.createElement("img");
+    mainImg.className = "main-img";
+    mainImg.src = postPicUrl;
+    imgFrame.appendChild(mainImg);
+  };
 };
 
-getShibas();
+for (var i = 0; i < 5; i++) {
+  getShiba();
+}

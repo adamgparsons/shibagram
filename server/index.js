125a9e9ddb5867a113477e5e917000ae8020f436
Adam Parsons <adam@adamparsons.me>
2021-07-24 18:47:32 +0100

    added static route on server

--- server/index.js
+++ server/index.js
@@ -1,17 +1,22 @@
-const express = require('express');
-const routes = require('./routes')
+const express = require("express");
+const routes = require("./routes");
+const path = require("path");
 
 const app = express();
 
-
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
-app.use('/', routes);
+// Serve static files from the frontend
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
+app.use("/", routes);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../client/dist/index.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));

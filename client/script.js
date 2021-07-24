160bc8419b9c2e4424cc334f5c1b65a668701c79
Adam Parsons <adam@adamparsons.me>
2021-07-24 18:17:27 +0100

    move old shibagram into client dir

---
+++ client/script.js
@@ -0,0 +1,81 @@
+import japanCities from "./japanCities.mjs";
+import dogNames from "./dogNames.mjs";
+const mainTag = document.querySelector("main");
+const showMoreButton = document.querySelector(".show-more");
+
+// Number of posts on page load/ onclick load show more
+const numberOfPosts = 6;
+
+// API url
+
+const url =
+  window.location.hostname === "localhost"
+    ? `http://localhost:3000/count=${numberOfPosts * 2}`
+    : "https://badadvice.rest/api/";
+
+// feed of content
+let feed;
+
+showMoreButton.style.display = "none";
+
+const getShibas = function () {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      // JSON data into two arrays
+      // One for display pics
+      const displayPics = data.slice(0, numberOfPosts);
+      // One for post pics
+      const postPics = data.slice(numberOfPosts, data.length);
+
+      // create an array of objects
+      feed = displayPics.map((displayPic) => {
+        const postNumber = displayPics.indexOf(displayPic);
+
+        // Generate random dog name
+        const dogName = dogNames[Math.floor(Math.random() * dogNames.length)];
+
+        // Generate random dog location
+        const postLocation =
+          japanCities[Math.floor(Math.random() * japanCities.length)] +
+          ", Japan";
+
+        // populate the object
+        const postObj = {};
+        postObj.displayPic = displayPics[postNumber];
+        postObj.postPic = postPics[postNumber];
+        postObj.dogName = dogName;
+        postObj.postLocation = postLocation;
+        return postObj;
+      });
+      displayFeed();
+    });
+};
+
+const displayFeed = function () {
+  feed.forEach((feedItem) => {
+    const post = `
+    <div class="shiba-post">
+      <div class="profile-bar">
+        <div class="profile-img-holder">
+          <div class="profile-img" style="background-image: url(${feedItem.displayPic});"></div>
+        </div>
+        <div class="profile-details">
+          <h2>${feedItem.dogName}</h2>
+          <h3>${feedItem.postLocation}</h3>
+        </div>
+      </div>
+      <div class="img-frame">
+        <img class="main-img" src="${feedItem.postPic}">
+      </div>
+    </div>
+    `;
+    mainTag.innerHTML = mainTag.innerHTML + post;
+  });
+  // display show more button
+  showMoreButton.style.display = "block";
+};
+
+getShibas();
+
+showMoreButton.addEventListener("click", getShibas);

b5254c2601af0ee35ab7c88a81f58fb4955ffcda
Adam Parsons <adam@adamparsons.me>
2021-07-25 16:04:36 +0100

    remove unecessary line in router

--- server/routes.js
+++ server/routes.js
@@ -3,8 +3,6 @@ const controller = require("./controller");
 
 const router = Router();
 
-// router.get('/', adviceController.advice_get);
-
 router.get("/count=:count", controller.shiba_get);
 
 module.exports = router;
